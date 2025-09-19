780412e312958f1657bc1084b51a2a3b39d8bf04
mdma420 <124081182+mdma420@users.noreply.github.com>
2025-09-19 18:48:09 +0700

    clear file

--- src/services/package.service.ts
+++
@@ -1,9 +0,0 @@
-import api from "./http";
-import { url_get_package_by_type } from "./constants";
-
-export type PackageItem = { id: string; name: string; price: number };
-
-export async function getPackagesByType(type: string): Promise<PackageItem[]> {
-  const res = await api.get(url_get_package_by_type(), { params: { type } });
-  return res.data?.items ?? [];
-}
--- src/styles/Colors.tsx
+++
@@ -1,6 +0,0 @@
-const color = {
-  main: "#3D90D7",
-  white: "#FFFFFF",
-}
-
-export default color;
